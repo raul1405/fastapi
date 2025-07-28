4a0d20778ce4e52ae9bee7458eacf81d861296d1
raul1405 <32872722+raul1405@users.noreply.github.com>
2025-07-28 17:06:27 +0200

    Update and rename __init__.py to lpislib/__init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-lpislib/
-  __init__.py
-  WuLpisApiClass.py   # copy from https://github.com/alexanderhofstaetter/wu-lpis-api
-main.py
-requirements.txt
---
+++ lpislib/__init__.py
@@ -0,0 +1,1 @@
+from .WuLpisApiClass import WuLpisApi
